bb5ce26371d169baaf9f1d9a9a3436cdff636d4c
Andre Rego <andremregodev@gmail.com>
2022-07-10 00:10:14 -0300

    refactor: remove babel dependencies

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  presets: [
-    "@babel/preset-env",
-    "@babel/preset-react",
-  ],
-};
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+//This plugin prevents packages listed in peerDependencies from being bundled with our component library
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+//efficiently bundles third party dependencies we've installed and use in node_modules
+import resolve from '@rollup/plugin-node-resolve';
+
+// //enables transpilation into CommonJS (CJS) format
+import commonjs from '@rollup/plugin-commonjs';
+
+//transpiled our TypeScript code into JavaScript. This plugin will use all the settings we have set in tsconfig.json.
+//We set "useTsconfigDeclarationDir": true so that it outputs the .d.ts files in the directory specified by in tsconfig.json
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import dts from 'rollup-plugin-dts';
