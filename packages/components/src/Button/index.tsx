ace1901c3cb2bbd40a0d44b0f9bd44838d6f05ab
Andre Rego <andremregodev@gmail.com>
2022-07-09 23:09:25 -0300

    feat: Add background color to button

--- packages/components/src/Button/index.tsx
+++ packages/components/src/Button/index.tsx
@@ -5,10 +5,11 @@ import * as S from "./styles"
 export interface IButton  {
   children: React.ReactNode | string;
   color?: string;
+  backgroundColor?: string;
 }
 
-const Button = ({ children, color }: IButton) => {
-  return <S.Button style={{color}}>{children}</S.Button>
+const Button = ({ children, color, backgroundColor }: IButton) => {
+  return <S.Button style={{color, backgroundColor }}>{children}</S.Button>
 }
 
 export { Button }
